0c3573c7736f282dd5d7b09ef5a05600edb54d61
Martijn <martijn.dull@student.hu.nl>
2018-12-09 16:20:14 +0100

    First prototype for quiz part of the game
    First prototype for quiz part of the game

---
+++ Assets/Scripts/Quiz/DataController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DataController : MonoBehaviour{
+    public RoundData[] allRoundData;
+
+    public RoundData GetCurrentRoundData()
+    {
+        return allRoundData[0];
+    }
+
+}
