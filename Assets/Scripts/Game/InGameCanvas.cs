82b70e591c2af2bf426a7c9bfb8b3072653da1e3
NielsvB95 <niels.van.baren@live.nl>
2019-03-17 16:25:59 +0100

    Added quitbutton in game

--- Assets/Scripts/Game/InGameCanvas.cs
+++ Assets/Scripts/Game/InGameCanvas.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using System.Net;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 
 public class InGameCanvas : MonoBehaviour {
 
     APIManager apimanager;
+    public GameObject popupScreen;
 
     public void OpenInventoryMenu()
     {
         InventoryMenu.InventoryCheck = true;
     }
-
-    void Start()
+    public void OpenPopup()
     {
+        popupScreen.SetActive(true);
     }
-    
+    public void Resume()
+    {
+        popupScreen.SetActive(false);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
